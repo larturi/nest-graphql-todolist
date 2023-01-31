70284a931db4d8a2e4a62f7ccdcd1030f85418b4
Leandro Arturi <lea.arturi@gmail.com>
2023-01-31 17:46:03 -0300

    GraphQL Configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { HelloWorldModule } from './hello-world/hello-world.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    HelloWorldModule,
+  ],
   controllers: [],
   providers: [],
 })
