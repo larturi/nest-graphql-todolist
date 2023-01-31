d118689b6a8ae86c7e7833b1daa8a5bed5dbf896
Leandro Arturi <lea.arturi@gmail.com>
2023-01-31 17:50:23 -0300

    Install ApolloServer Plugin

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { Module } from '@nestjs/common';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 import { HelloWorldModule } from './hello-world/hello-world.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      // playground: false,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
     }),
     HelloWorldModule,
   ],
