7e8972403a148162ee9add9f15b4a5b68875349e
Leandro Arturi <lea.arturi@gmail.com>
2023-02-01 12:46:26 -0300

    getRandomNumber and getRandomFromZeroTo methods

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,27 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver, Int, Args } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, { description: 'Retorna hola mundo', name: 'hello' })
   helloWorld(): string {
     return 'Hello World!';
+  }
+
+  @Query(() => Float, {
+    description: 'Retorna un numero random',
+    name: 'randomNumber',
+  })
+  getRandomNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, {
+    description: 'Retorna un numero random entre cero y n (default = 10)',
+    name: 'randomFromZeroTo',
+  })
+  getRandomFromZeroTo(
+    @Args('to', { nullable: true, type: () => Int }) n = 10,
+  ): number {
+    return Math.floor(Math.random() * (n + 1));
   }
 }
